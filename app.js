cc2e438197f0cecd80a507c6ef08476154c14869
Hassan Mehmood <hmehmood411@gmail.com>
2020-06-20 21:26:06 +0500

    Assignment # 6

---
+++ app.js
@@ -0,0 +1,200 @@
+//========================== Chapter 21-25 ===========================
+//Task # 1
+
+/*
+var firstName = "Hassan"; //=================require some modification
+var lastName = "Mehmood"; //=================require some modification
+var fullName = firstName + " "+lastName;
+document.write("Hello, " +fullName+"<br>");
+
+//Task # 2
+var phoneModel = " Samsung Galaxy S6 Edge Plus";
+document.write("My favourite phone is: "+ phoneModel+"<br>");
+document.write("Length of string: "+ phoneModel.length+"<br>");
+
+//Task # 3
+var letter = "Pakistani"
+document.write("String: "+letter +"<br>");
+document.write("Index of 'n': "+letter.indexOf("n") +"<br>");
+
+//Task # 4
+var letter = "Hello World"
+document.write("String: "+letter +"<br>");
+document.write("last Index of 'l': "+letter.indexOf("l",4) +"<br>");
+
+//Task # 5
+var letter = "Pakistani"
+document.write("String: "+letter +"<br>");
+document.write("Character at index 3: "+letter[3] +"<br>");
+
+//Task # 6
+var firstName = "Hassan ";
+var lastName = "Mehmood";
+var fullName = firstName.concat(lastName)
+document.write("Hello, " +fullName+"<br>");
+
+//Task # 7
+var city = "Hyderabad"
+var rep = city.replace("Hyder", "Islam"); 
+document.write("City: "+city +"<br>");
+document.write("After replacement: "+rep +"<br>");
+
+//Task # 8
+var message = "Ali and Sami are best friends. They play cricket and football together";
+var repl = message.replace("and","&");  //=================require some modification
+document.write("Message before replacement: "+message +"<br>");
+document.write("After replacement: "+repl +"<br>");
+
+//task # 9
+var str1 = "472"
+document.write("Value: " +str1+"<br>");
+document.write("Type: " +typeof(str1)+"<br>");
+var num1 = parseInt(str1)
+document.write("Value: " +num1+"<br>");
+document.write("Type: " +typeof(num1)+"<br>");
+
+
+//Task # 10
+var inp1 = "peanuts";
+document.write("User Input: " +inp1+"<br>");
+document.write("Upper case: " +inp1.toUpperCase()+"<br>");
+
+//task # 11
+var inp1 = "javascript";
+document.write("User Input: " +inp1+"<br>");
+document.write("Title case: " +inp1.toLocaleUpperCase(5)+"<br>");//=================require some modification
+
+
+//Task # 12
+var num = 35.36;
+document.write("Number: " +num+"<br>");
+num = num.toString();
+document.write("Result: " +num.trim(2)+"<br>");//=================require some modification
+
+//rask # 13
+//=================require some modification
+
+
+//Task # 14
+var A = ["cake","apple pie","cookie","chips","patties"];
+var userInp = "apple PIE";
+if (A.includes(userInp.toLowerCase()) ){
+    document.write(userInp+" is available at index "+A.indexOf(userInp.toLocaleLowerCase())+" in our bakery <br>");
+}//=================require some modification
+else{
+   document.write("We are sorry. "+userInp+" is not available in our bakery"+"<br>");
+}
+
+
+//Task # 15 
+//=================require some modification
+
+
+//Task # 16
+var university = "University of Karachi";
+var arrUni = university.split("");
+for (var i=0; i<arrUni.length; i++){
+    document.write(arrUni[i]+"<br>");
+}
+
+
+//Task # 17
+var inp = "Pakistan";
+document.write("User Input: " +inp+"<br>");
+document.write("Last Character of input: " +inp[inp.length-1]+"<br>");
+
+//Task # 18
+var str1 = "The quick brown fox jumps over the lazy dog";
+document.write("Text: " +str1+"<br>");
+document.write("There are " +str1.split("the").length +" occurrence(s) of word 'the'<br>");
+
+
+
+
+//========================== Chapter 26-30 ===========================
+//Task # 1
+var num = 3.45214;
+document.write("number: " +num+"<br>");
+document.write("round off value: " +Math.round(num)+"<br>");
+document.write("floor value: " +Math.floor(num)+"<br>");
+document.write("ceil value: " +Math.ceil(num)+"<br>");
+
+
+//Task # 2
+var num = -2.673;
+document.write("number: " +num+"<br>");
+document.write("round off value: " +Math.round(num)+"<br>");
+document.write("floor value: " +Math.floor(num)+"<br>");
+document.write("ceil value: " +Math.ceil(num)+"<br>");
+
+//Task # 3
+var num = -4;
+document.write("The absolute value of "+num+" is "+Math.abs(num)+"<br>");
+
+
+//Task # 4
+var randNum = Math.random()*10;
+var randNum1 = Math.round(randNum)
+if (randNum1<=6){
+    document.write("random dice value: " +randNum1+"<br>");
+}
+
+
+//Task # 5
+var toss = Math.random();
+toss = Math.round(toss);
+document.write("Random Coin values: " +parseInt(toss+1)+"<br>");
+toss = Math.random();
+toss = Math.round(toss);
+document.write("Random Coin values: " +parseInt(toss+1)+"<br>");
+
+
+//Task # 6
+var ranNum = Math.random()*100;
+ranNum = Math.round(ranNum);
+document.write("Random number between 1 and 100: " +ranNum+"<br>");
+
+//Task # 7
+var weight = 50.3;
+document.write("The weight of user is " +weight+" Kilograms<br>");
+
+//Task # 8
+var secNum = Math.floor((Math.random() * 10) + 1);
+var guessNum = parseInt(prompt("Enter the guessing number: "));
+if (secNum==guessNum){
+    document.write("Bingo! Correct Answer ");
+}
+else if(secNum==guessNum+1){
+    document.write("Close enough to the correct answer");
+};
+
+*/
+//========================== Chapter 31-34 ===========================
+//Task # 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
